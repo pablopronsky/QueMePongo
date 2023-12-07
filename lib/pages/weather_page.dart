2b9c4a7291bd3fd8ff90085b8a39335fa8b2a7ac
pablopronsky <pronskypablo@gmail.com>
2023-12-07 14:00:17 -0300

    funcionalidades basicas andando, encuentra ubicacion y muestra temperatura

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,8 @@
+import 'package:dev/service/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../model/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +12,69 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('bf60016fc2c512c3497f432fc12a01ae');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try{
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }catch (e) {
+      return AlertDialog(
+        title: const Text('Error'),
+        content: Text(e.toString()),
+      );
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition != null) mainCondition = mainCondition.toLowerCase();
+    switch(mainCondition) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'dust':
+      case 'fog':
+        return 'assets/nublado.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/lluvia.json';
+      case 'thunderstorm':
+        return 'assets/tormenta_electrica.json';
+      case 'clear':
+        return 'assets/soleado.json';
+      default:
+        return 'assets/sonrisa.json';
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // busca el tiempo apenas se abre la app
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Buscando tu ubicación..."),
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+            Text('${_weather?.temperature.round()}ºC'),
+            Text(_weather?.mainCondition ?? "")
+          ],
+        ),
+      )
+    );
   }
 }
---
+++ lib/service/weather_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../model/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService{
+  static const URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async{
+    final response = await http.get(Uri.parse('$URL?q=$cityName&appid=$apiKey&units=metric&lang=es'));
+    if (response.statusCode != 200) {
+      throw Exception('Error al obtener el clima: ${response.statusCode}');
+    }
+    if(response.statusCode == 200){
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  Future <String> getCurrentCity() async {
+    // busca permisos para acceder a la ubicación
+    LocationPermission locationPermission = await Geolocator.checkPermission();
+    // si no los consigue, vuelve a intentar
+    if(locationPermission == LocationPermission.denied) {
+      locationPermission = await Geolocator.checkPermission();
+    }
+    // accede a la ubicación del usuario
+    Position userPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    // coordenadas
+    List<Placemark> placemarks = await placemarkFromCoordinates(userPosition.latitude, userPosition.longitude);
+    // a traves de las placemarks de arriba consigue el nombre de la localidad
+    String? city = placemarks[0].locality;
+    return city ?? "";
+  }
+}
